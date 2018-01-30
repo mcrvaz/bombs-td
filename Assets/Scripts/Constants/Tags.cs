3326af832dc803c64b65a071d69977054c565d0d
Marcello Vaz <marcello.crvaz@gmail.com>
2018-01-29 22:38:00 -0200

    feat: tower animations and positioning

--- Assets/Scripts/Constants/Tags.cs
+++ Assets/Scripts/Constants/Tags.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public static class Tags {
 	public static string ENEMY = "Enemy";
 	public static string OBJECTIVE = "Objective";
+	public static string TOWER_CONTAINER = "TowerContainer";
 }
---
+++ Assets/Scripts/TowerDefense/TowerBuilder/TowerBuilder.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TowerBuilder : MonoBehaviour {
+
+	public GameObject selectedTower;
+
+	private Vector3 mousePosition;
+	private GameObject placeholder;
+	private SpriteRenderer sprite;
+	private GameObject towerContainer;
+	private bool canBuild;
+
+	void Awake() {
+		towerContainer = GameObject.FindGameObjectWithTag(Tags.TOWER_CONTAINER);
+	}
+
+	void Start() {
+		UpdateMousePosition();
+		var spriteObject = selectedTower.transform.GetChild(0).gameObject;
+		placeholder = (GameObject) Instantiate(spriteObject, mousePosition, Quaternion.identity);
+		placeholder.transform.SetParent(transform);
+		sprite = placeholder.GetComponent<SpriteRenderer>();
+	}
+
+	void Update() {
+		UpdateMousePosition();
+		UpdatePlaceholderPosition();
+		IsValidPosition(placeholder.transform.position);
+		PaintTower(canBuild);
+		if (Input.GetMouseButtonDown(0)) BuildTower();
+    }
+
+	void UpdateMousePosition() {
+		mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+
+	void UpdatePlaceholderPosition() {
+		if(placeholder != null) {
+			placeholder.transform.position = new Vector3(
+				RoundToHalf(mousePosition.x),
+				RoundToHalf(mousePosition.y),
+				0
+			);
+		}
+	}
+
+	float RoundToHalf(float v) {
+        const float halfInverse = 1/0.5f;
+		return (float) (Math.Round(v * halfInverse, MidpointRounding.AwayFromZero) / halfInverse);
+	}
+
+	void BuildTower() {
+		if(!canBuild) return;
+		var go = (GameObject) Instantiate(selectedTower, placeholder.transform.position, Quaternion.identity);
+		go.transform.SetParent(towerContainer.transform);
+	}
+
+	bool IsValidPosition(Vector3 position) {
+		var hitCollider = Physics2D.OverlapBox(position, Vector2.one / 2, 0, LayerMask.GetMask("Towers"));
+		canBuild = hitCollider == null;
+		return canBuild;
+	}
+
+	void PaintTower(bool canBuild) {
+		if(canBuild) sprite.color = Color.green;
+		else sprite.color = Color.red;
+	}
+
+}
